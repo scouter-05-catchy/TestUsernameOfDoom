72b7b89f3ec6d20ed21407cf704c4f44ab223cb8
Daniel Yount <danielyount@apple.com>
2022-08-05 15:05:28 -0700

    Initial Commit

---
+++ TestUsernameOfDoom/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TestUsernameOfDoom
+//
+//  Created by Daniel Yount on 8/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestUsernameOfDoom/TestUsernameOfDoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestUsernameOfDoomApp.swift
+//  TestUsernameOfDoom
+//
+//  Created by Daniel Yount on 8/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TestUsernameOfDoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
